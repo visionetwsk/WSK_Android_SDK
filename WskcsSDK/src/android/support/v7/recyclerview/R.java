e24e049679114b33112a0460279eddd2bb0a91e2
zhenghai <zhenghai@visionet.com.cn>
2016-11-17 16:43:07 +0800

    first commit

---
+++ WskcsSDK/src/android/support/v7/recyclerview/R.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package android.support.v7.recyclerview;
+
+/**
+ * 
+ * @author 郑海
+ * @version 1.0 创建时间2016-11-15 下午5:25:45
+ */
+public class R {
+	public static final class id {
+		public static int item_touch_helper_previous_elevation = com.visionet.wskcss.R.id.item_touch_helper_previous_elevation;
+	}
+
+	public static final class dimen {
+		public static int item_touch_helper_max_drag_scroll_per_frame = com.visionet.wskcss.R.dimen.item_touch_helper_max_drag_scroll_per_frame;
+	}
+
+	public static final class styleable {
+		public static final int[] RecyclerView = com.visionet.wskcss.R.styleable.RecyclerView;
+
+		public static int RecyclerView_layoutManager = com.visionet.wskcss.R.styleable.RecyclerView_layoutManager;
+
+		public static int RecyclerView_reverseLayout = com.visionet.wskcss.R.styleable.RecyclerView_reverseLayout;
+		public static int RecyclerView_spanCount = com.visionet.wskcss.R.styleable.RecyclerView_spanCount;
+		public static int RecyclerView_stackFromEnd = com.visionet.wskcss.R.styleable.RecyclerView_stackFromEnd;
+	}
+}
