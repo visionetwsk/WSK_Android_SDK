eab41eebc103b819fa61e4565998515b7e839d98
zhenghai <zhenghai@visionet.com.cn>
2016-12-12 13:33:39 +0800

    添加 可设置userinfo

--- WSK_Android_SDK_forEclipse/WskcsSDKTest/src/com/example/test/MainActivity.java
+++ WSK_Android_SDK_forEclipse/WskcsSDKTest/src/com/example/test/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.test;
 
 
-import com.visionet.wskcss.WskCS;
-import com.visionet.wskcss.bean.WskCsUIConfig;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+
+import com.visionet.wskcss.WskCS;
+import com.visionet.wskcss.bean.UserInfo;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		WskCS.init( this,"dmlzaW9uZXQ6TW9iaWxlQ2hhdFRlc3Qx");
-		 WskCsUIConfig config = new WskCsUIConfig();
-	        config.setBackIconDrweableId(R.drawable.selector_test11);
-	        WskCS.configUI(config);
+//		WskCS.init( this,"dmlzaW9uZXQ6TW9iaWxlQ2hhdFRlc3Qx");
+//		 WskCsUIConfig config = new WskCsUIConfig();
+//	        config.setBackIconDrweableId(R.drawable.selector_test11);
+//	        WskCS.configUI(config);
+		UserInfo userInfo = new UserInfo(22+"");
+        userInfo.setCustomerName( "zhenghai" );
+        userInfo.setEmail( "www.zhenghai@visionet.com.cn" );
+        userInfo.setGender( 1);
+        userInfo.setAddress( "上海延安西路1146号" );
+        userInfo.setBirthday( "1998-03-03" );
+        userInfo.setHeadimgurl( "http://avatar.csdn.net/E/8/0/1_oyangyujun.jpg" );
+        WskCS.init( this,"dmlzaW9uZXQ6TW9iaWxlQ2hhdFRlc3Qx",userInfo );
 		setContentView(R.layout.activity_main);
 	}
 
